556e573e8d1643418412d9c508402ccbc7e572f8
lucedeswede <nathanielcarolina@gmail.com>
2019-12-02 16:50:10 +0100

    Modify navbar title, search button text, and formatting

--- src/navbar.js
+++ src/navbar.js
@@ -4,11 +4,11 @@ class Navbar extends Component {
     render() {
         return (
             <nav className="navbar navbar-dark bg-success">
-                <a className="navbar-brand" href="#">HR Management System: SNARS</a>
+                <a className="navbar-brand" href="#">SNARS Human Resources System</a>
                 <form className="form-inline my-2 my-lg-0">
-                <span className="navbar-text text-white mr-3">Employee Search:</span>
+                <span className="navbar-text text-white small text-uppercase mr-3">Employee Search:</span>
                 <input className="form-control mr-sm-2" type="search" placeholder="Enter Employee ID" aria-label="Search" />
-                <button className="btn btn-outline-light my-2 my-sm-0" type="submit">Submit</button>
+                <button className="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
                 </form>
             </nav>
         );
