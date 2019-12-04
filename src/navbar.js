1576b739fc3e90cca0c781b69c6fe2128e983434
lucedeswede <nathanielcarolina@gmail.com>
2019-12-04 23:09:18 +0100

    Modify search placeholder to: Enter Name or ID

--- src/navbar.js
+++ src/navbar.js
@@ -7,7 +7,7 @@ class Navbar extends Component {
                 <a className="navbar-brand" href="#">SNARS Human Resources System</a>
                 <form className="form-inline my-2 my-lg-0">
                 <span className="navbar-text text-white small text-uppercase mr-3">Employee Search:</span>
-                <input className="form-control mr-sm-2" type="search" placeholder="Enter Employee ID" aria-label="Search" />
+                <input className="form-control mr-sm-2" type="search" placeholder="Enter Name or ID" aria-label="Search" />
                 <button className="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
                 </form>
             </nav>
